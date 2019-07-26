aea92f22c18a53d066e7ab0738e073b261c2bc78
Joerg Kuetemeier <joerg@kuetemeier.de>
2019-07-26 21:07:00 +0200

    New Installer version added.

--- install.sh
+++ install.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+#-------
+# clone
+
 git clone --bare git@s.kuetemeier.net:home-files $HOME/.home-files
+
+#---------------
+# home function
 
 function home {
    /usr/bin/git --git-dir=$HOME/.home-files/ --work-tree=$HOME $@
 }
 
-# create .home-local and local backup folder
+#--------
+# backup
+
+echo "Creating .home-local"
+# and backup-folder
 mkdir -p .home-local/backup
 
-# checkout repo
-home checkout
+echo "Backing up home-files."
 
-if [ $? = 0 ]; then
-  echo "Checked out home-files.";
-  else
-    echo "Backing up pre-existing dot files.";
-    home checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .home-local/backup/{}
-fi;
+FILES=`home ls-tree --name-only -r master .`
 
-home checkout
+for f in $FILES ; do
+  if [ -f $HOME/$f ]; then
+    echo -n "... backing up: "
+    echo $f
+    mv $HOME/$f .home-local/backup/$f
+  fi
+done
+
+#----------
+# checkout
+
+home ls-tree --name-only -r master . | xargs git --git-dir=$HOME/.home-files/ --work-tree=$HOME checkout master --
+
 home config status.showUntrackedFiles no
